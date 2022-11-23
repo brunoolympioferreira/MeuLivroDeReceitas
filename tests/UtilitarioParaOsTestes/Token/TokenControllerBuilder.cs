85dc0d0244d8f97936ab73d42d6c4f4115226867
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-11-23 18:25:18 -0300

    adicionando testes de unidade de usecase registrar usuario

---
+++ tests/UtilitarioParaOsTestes/Token/TokenControllerBuilder.cs
@@ -0,0 +1,12 @@
+﻿using MeuLivroDeReceitas.Application.Servicos.Criptografia;
+using MeuLivroDeReceitas.Application.Servicos.Token;
+
+namespace UtilitarioParaOsTestes.Token;
+
+public class TokenControllerBuilder
+{
+    public static TokenController Instancia()
+    {
+        return new TokenController(1000, "TTNTUjUjQkpeSXJVJnE4IzU1RUVlSHdRSWxna2hkOXZSJjgzWDIkR2ZoQ3pUZWI0THM=");
+    }
+}
