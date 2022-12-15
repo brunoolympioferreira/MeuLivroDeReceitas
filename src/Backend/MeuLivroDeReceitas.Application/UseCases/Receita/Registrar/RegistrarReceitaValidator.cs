dcdc3f7c25d659e3d69c1b507236f58671bd1960
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-12-15 14:58:36 -0300

    feat: implementando testes de unidade para registrar receita validator

--- src/Backend/MeuLivroDeReceitas.Application/UseCases/Receita/Registrar/RegistrarReceitaValidator.cs
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Receita/Registrar/RegistrarReceitaValidator.cs
@@ -7,23 +7,6 @@ public class RegistrarReceitaValidator : AbstractValidator<RequisicaoRegistrarRe
 {
 	public RegistrarReceitaValidator()
 	{
-		RuleFor(x => x.Titulo).NotEmpty();
-		RuleFor(x => x.Categoria).IsInEnum();
-		RuleFor(x => x.ModoPreparo).NotEmpty();
-		RuleFor(x => x.Ingredientes).NotEmpty();
-		RuleForEach(x => x.Ingredientes).ChildRules(ingrediente =>
-		{
-			ingrediente.RuleFor(x => x.Produto).NotEmpty();
-			ingrediente.RuleFor(x => x.Quantidade).NotEmpty();
-		});
-
-		RuleFor(x => x.Ingredientes).Custom((ingredientes, contexto) =>
-		{
-			var produtosDistintos = ingredientes.Select(c => c.Produto).Distinct();
-            if (produtosDistintos.Count() != ingredientes.Count())
-			{
-				contexto.AddFailure(new ValidationFailure("Ingredientes", ""));
-			}
-		});
-	}
+        RuleFor(x => x).SetValidator(new ReceitaValidator());
+    }
 }
