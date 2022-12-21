f1aabfe8eb8fc254f7d483772de18121266ff890
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-12-21 08:43:44 -0300

    feat: criando classes repositorio para recuperar todas receitas do usuario

--- src/Backend/MeuLivroDeReceitas.Infraestructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
+++ src/Backend/MeuLivroDeReceitas.Infraestructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
@@ -1,14 +1,21 @@
 ﻿using MeuLivroDeReceitas.Domain.Entidades;
 using MeuLivroDeReceitas.Domain.Repositorios.Receita;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeuLivroDeReceitas.Infraestructure.AcessoRepositorio.Repositorio;
-public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio
+public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnlyRepositorio
 {
     private readonly MeuLivroDeReceitasContext _contexto;
     public ReceitaRepositorio(MeuLivroDeReceitasContext contexto)
     {
         _contexto = contexto;
     }
+
+    public async Task<IList<Receita>> RecuperarTodasDoUsuario(long usuarioId)
+    {
+        return await _contexto.Receitas.Where(r => r.UsuarioId == usuarioId).ToListAsync();
+    }
+
     public async Task Registrar(Receita receita)
     {
         await _contexto.Receitas.AddAsync(receita);
