f4f743ba76ef341c28f1bb76abccf6a92494a484
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-01-06 16:01:44 -0300

    feat: implementando tempo de preparo nas receitas

---
+++ src/Backend/MeuLivroDeReceitas.Infraestructure/Migrations/Versoes/Versao0000003.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace MeuLivroDeReceitas.Infraestructure.Migrations.Versoes;
+
+[Migration((long)NumeroVersoes.AlterarTabelaReceitas, "Adicionando coluna tempo para o preparo")]
+public class Versao0000003 : Migration
+{
+    public override void Down()
+    {
+    }
+
+    public override void Up()
+    {
+        Alter.Table("receitas").AddColumn("TempoPreparo").AsInt32().NotNullable().WithDefaultValue(0);
+    }
+}
