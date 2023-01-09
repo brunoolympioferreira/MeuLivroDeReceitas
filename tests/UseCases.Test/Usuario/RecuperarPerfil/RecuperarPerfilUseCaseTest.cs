a5e79dc726939eb51465c8a888d8b9136ac12846
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-01-09 09:08:52 -0300

    feat: testes de unidade recuperar perfil usuario

---
+++ tests/UseCases.Test/Usuario/RecuperarPerfil/RecuperarPerfilUseCaseTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using MeuLivroDeReceitas.Application.UseCases.Usuario.RecuperarPerfil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilitarioParaOsTestes.Entidades;
+using UtilitarioParaOsTestes.Mapper;
+using UtilitarioParaOsTestes.UsuarioLogado;
+using Xunit;
+
+namespace UseCases.Test.Usuario.RecuperarPerfil;
+public class RecuperarPerfilUseCaseTest
+{
+    [Fact]
+    public async Task Validar_Sucesso()
+    {
+        (var usuario, _) = UsuarioBuilder.Construir();
+
+        var useCase = CriarUseCase(usuario);
+
+        var resposta = await useCase.Executar();
+
+        resposta.Should().NotBeNull();
+        resposta.Nome.Should().Be(usuario.Nome);
+        resposta.Email.Should().Be(usuario.Email);
+        resposta.Telefone.Should().Be(usuario.Telefone);
+    }
+
+    private static RecuperarPerfilUseCase CriarUseCase(MeuLivroDeReceitas.Domain.Entidades.Usuario usuario)
+    {
+        var mapper = MapperBuilder.Instancia();
+        var usuarioLogado = UsuarioLogadoBuilder.Instancia().RecuperarUsuario(usuario).Construir();
+
+        return new RecuperarPerfilUseCase(mapper, usuarioLogado);
+    }
+}
